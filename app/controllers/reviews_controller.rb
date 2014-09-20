3a0cf68e012ee5ecd8bdea32fdcdc97f8e755b1c
rachelbookhout <rbookhou@gmail.com>
2014-09-20 10:30:21 -0400

    Can submit reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,18 +3,22 @@ class ReviewsController < ApplicationController
   def new
   #binding.pry
   #@restaurant = Restaurant.find(params[:id])
+  @restaurant = Restaurant.find(params[:restaurant_id])
   @review = Review.new
   end
 
   def create
-  @restaurant = Restaurant.find(params[:id])
+  #binding.pry
+  id = params[:review][:restaurant_id]
+  #@restaurant = Restaurant.find(params[:id])
   @review = Review.new(review_params)
-  @review.restaurant_id = @restaurant.id
-  @reviews = Review.all.order(created_at: :desc).where(restaurant_id:"#{@restaurant.id}")
-  if @restaurant.save == false
+  @review.restaurant_id = id
+  #@review.restaurant_id = @restaurant.id
+  #@reviews = Review.all.order(created_at: :desc).where(restaurant_id:"#{@restaurant.id}")
+  if @review.save == false
     render 'restaurants/show'
   else
-    redirect_to @restaurant
+  redirect_to restaurants_path
   end
   end
 
