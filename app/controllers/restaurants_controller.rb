17b3dbdad9312343abf4cbce2aa8459e666715d4
rachelbookhout <rbookhou@gmail.com>
2014-09-19 22:42:05 -0400

    Working new and create page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,26 +1,31 @@
 class RestaurantsController < ApplicationController
 
   def index
-  @restaurants = Restaurant.all
+    @restaurants = Restaurant.all
   #order them by when they were updated, show only 10
   end
 
   def show
-  @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
-  @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = Restaurant.new
   end
 
   def create
-  @restaurant = Restaurant.new(restaurant_params)
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to @restaurant
+      else
+        render :new
+      end
   end
 
 private
 
 def restaurant_params
-  params.require(:restaurant).permit(:name,:address,:city,:state,:zipcode)
+  params.require(:restaurant).permit(:name,:address,:city,:state,:zipcode,:description, :category)
 
 end
 end
